a7403c04b2bf55772b09e340a634b5271ba94462
Andrey <forestx24@gmail.com>
2022-05-20 16:32:09 +0200

    install firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HeaderModule } from './shared/components/header/header.module';
 import { EmployeeFormModule } from './shared/components/employee-form/employee-form.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderModule, EmployeeFormModule],
+  imports: [BrowserModule, AppRoutingModule, HeaderModule, EmployeeFormModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crud-employees-d383b',
+    appId: '1:1028944738076:web:4e008623bd82bc2d8fecd9',
+    storageBucket: 'crud-employees-d383b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD5hpoNbWcA1iI4Y_xPAbH-NF6H-AMnfb4',
+    authDomain: 'crud-employees-d383b.firebaseapp.com',
+    messagingSenderId: '1028944738076',
+  },
   production: false
 };
 
